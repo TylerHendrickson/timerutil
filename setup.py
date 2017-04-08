9b3c956cc9dc6b1643954e7affac44335606278d
TySkby <hendrickson.tsh@gmail.com>
2017-04-08 14:52:41 -0500

    Make distributable with setuptools

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='Timerutil',
+    version='1.0.0',
+    url='https://github.com/TySkby/timerutil',
+    author='Tyler Hendrickson',
+    author_email='hendrickson.tsh@gmail.com',
+    description='Collection of timer-related utilities for Python',
+    long_description=__doc__,
+    packages=['timerutil'],
+    zip_safe=True,
+    test_suite='tests',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
